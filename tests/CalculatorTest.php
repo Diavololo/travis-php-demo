f075f2f033ca9d1f3210976090c68698b7a0c235
Diavololo <andres.leon.tabares@correounivalle.edu.co>
2025-10-16 08:20:30 -0500

    Arreglo

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -9,7 +9,7 @@ final class CalculatorTest extends TestCase
     public function testAdd(): void
     {
         $calc = new Calculator();
-        $this->assertSame(5, $calc->add(2, 3);
+        $this->assertSame(5, $calc->add(2, 3));
         // Para forzar un fallo en la demo, cambia el 5 por 6 y haz push.
     }
 }
