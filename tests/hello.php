2ddd98f441df046cb4c33b72b7f7df7476e1cac4
Diavololo <andres.leon.tabares@correounivalle.edu.co>
2025-10-16 10:55:23 -0500

    hola

---
+++ tests/hello.php
@@ -0,0 +1,13 @@
+<?php
+// Colores ANSI: \033[ = escape code
+$rojo = "\033[1;31m";   // 1 = negrita, 31 = rojo
+$reset = "\033[0m";     // vuelve al color normal
+
+echo "\n\n";
+echo $rojo;
+echo "===============================\n";
+echo "       H O L A   M U N D O     \n";
+echo "===============================\n";
+echo $reset;
+echo "\n\n";
+?>
